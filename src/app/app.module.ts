a7b953b3b9348c3424e45478e20db40b28179e8d
Your Name <vudinhtoan1999@gmail.com>
2020-10-27 13:57:46 +0700

    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,32 @@
+import { ApiService } from 'src/app/lib/api.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { MainComponent } from './main/main.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ErrorInterceptor } from './lib/error.interceptor';
+import { JwtInterceptor } from './lib/jwt.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    MainComponent
+      
   ],
   imports: [
+    ApiService,
     BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
